d298c30dd0b008f5464a793699807ed1e64376be
yassr <yasserkun@gmail.com>
2020-05-24 12:26:13 -0300

    Feed instagram (without styles)

---
+++ js/api.js
@@ -0,0 +1,40 @@
+async function callingFn() {
+    try {
+        const response = await fetch("https://v1.nocodeapi.com/yfront/instagram/reQvBgmbSvVhwvgG?limit=5", {
+            method: "get",
+            headers: {
+                "Content-Type": "application/json"
+            }
+        });
+        const data = await response.json();
+        return data;
+       // console.log("Success:", JSON.stringify(json));
+    } catch (error) {
+        console.error("Error:", error);
+    }
+}
+
+
+function renderInsta(item){
+    return(
+    `
+    <figure>
+        <a target="_blank" href="${item.permalink}">
+            <img src="${item.media_url}" alt="${item.caption}">
+            <figcaption>${item.caption}</figcaption>
+            <div class="date">${item.timestamp}</div>
+        </a>
+    </figure>
+    `)
+}
+const $instaArea = document.querySelector('#instagram');
+async function loadInsta(){
+    const dataInsta = await callingFn();
+    dataInsta.data.forEach((item) => {
+         console.log(item)
+        const renderString = renderInsta(item);
+        $instaArea.innerHTML += renderString;
+    })
+} 
+loadInsta()
+
